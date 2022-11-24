431700eeaef90a067bd55162f319372edce0041c
dodamking <power6120@naver.com>
2022-11-24 19:10:00 +0900

    아이피 관련 다시 수정중

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,8 @@ const cors = require('cors')
 const history = require('connect-history-api-fallback')
 const helmet = require('helmet')
 const request = require('request')
+const geoip = require('geoip-lite')
+const requestIp = require('request-ip')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -32,23 +34,36 @@ app.use(helmet())
 // app.use(cors(corsOption))
 
 app.use((req, res, next) => {
-  request('https://api.ip.pe.kr/json/', function(error, response, body){
-    if(error){
-      console.log(error);
-      return;
-    }
-    if(!error && response.statusCode==200){
-      const ip = JSON.parse(body).ip
-      console.log('접속 시도 지역:', JSON.parse(body).country_name.ko)
-      console.log('접속 시도 ip:', ip)
-      const country_code = JSON.parse(body).country_code;
-      if(country_code !== 'KR'){
-        res.send("Access Denied")
-        return;
-      }
-    }
-    next()
-  })
+  console.log(requestIp.getClientIp(req).substring(7))
+  console.log(geoip.lookup("207.97.227.239"))
+  // const ip = JSON.parse(body).ip
+  // console.log('접속 시도 지역:', JSON.parse(body).country_name.ko)
+  // console.log('접속 시도 ip:', ip)
+  // const country_code = JSON.parse(body).country_code;
+  // if(country_code !== 'KR'){
+  //   res.send("Access Denied")
+  //   return;
+  // }
+
+  // request('https://api.ip.pe.kr/json/', req.ip, function(error, response, body){
+  //   if(error){
+  //     console.log(error);
+  //     return;
+  //   }
+  //   if(!error && response.statusCode==200){
+  //     console.log(body);
+  //     const ip = JSON.parse(body).ip
+  //     console.log('접속 시도 지역:', JSON.parse(body).country_name.ko)
+  //     console.log('접속 시도 ip:', ip)
+  //     const country_code = JSON.parse(body).country_code;
+  //     if(country_code !== 'KR'){
+  //       res.send("Access Denied")
+  //       return;
+  //     }
+  //   }
+  //   next()
+  // })
+  next()
 })
 
 app.use('/', indexRouter);
